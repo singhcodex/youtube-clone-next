6ad43a7e6d3f2c1ed40233f7f0ef7cf4b1e0afd5
singhcodex <singh.hessen@gmail.com>
2023-05-03 11:44:58 +0200

    after random data created

--- pages/api/utils.js
+++ pages/api/utils.js
@@ -19,6 +19,36 @@ export default async function handler(req, res){
             })
             usersCount++
         }
+
+        const videoUrl = 'https://bootcamp-singh.fra1.digitaloceanspaces.com/SampleVideo_1280x720_2mb.mp4'
+        const thumbnailUrl = 'https://bootcamp-singh.fra1.digitaloceanspaces.com/azamat-e-ooCoR8x0MmY-unsplash.jpg'
+
+        const users = await prisma.user.findMany()
+
+        const getRandomUser = () => {
+            const randomIndex = Math.floor(Math.random() * users.length)
+
+            return users[randomIndex]
+        }
+
+        let videoCount = 0
+
+        while(videoCount < 20) {
+            await prisma.video.create({
+                data: {
+                    title: faker.lorem.words(),
+                    thumbnail: thumbnailUrl,
+                    url: videoUrl,
+                    length: faker.datatype.number(1000),
+                    visibility: 'public',
+                    views: faker.datatype.number(1000),
+                    author: {
+                        connect: {id: getRandomUser().id },
+                    }
+                },
+            })
+            videoCount++
+        }
     }
 
 
